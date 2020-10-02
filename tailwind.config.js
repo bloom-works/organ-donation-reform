75b02fec4dea07ac82e62d9149181f4f75ca453a
Hondo <hi@hondo.xyz>
2020-10-01 21:26:22 -0400

    Adds better CSS build, cleans up some stuff

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        darkGray: '#363737',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
